e599fc1c7f5650944abd294b3697e715e2a1b2e4
Steven <gonghaima@hotmail.com>
2020-02-11 21:14:44 +1100

    two more test case - failing

--- PrimeService.Tests/PrimeService_IsPrimeShould.cs
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -19,5 +19,16 @@ namespace Prime.UnitTests.Services
 
             Assert.False(result, "1 should not be prime");
         }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.False(result, $"{value} should not be prime");
+        }
     }
 }
